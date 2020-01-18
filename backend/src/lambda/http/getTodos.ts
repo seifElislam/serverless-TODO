28576f44a3443dbb9c2332e9093841037ef76af4
seifElislam <seif.eleslam1990@gmail.com>
2020-01-19 01:23:54 +0200

    Feat: Add getTodos function logic

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,35 @@
 import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getAllTodos } from "../../businessLogic/todos";
+import { parseUserId } from '../../auth/utils';
+import { createLogger } from '../../utils/logger';
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const logger = createLogger('getTodo');
+
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
-}
+  console.log('Processing event: ', event)
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(' ');
+  const jwtToken = split[1];
+  const userId = parseUserId(jwtToken);
+
+  const todos = await getAllTodos(userId);
+  logger.info(`get all Todo for user ${userId}`);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        items: todos,
+    }),
+};
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
