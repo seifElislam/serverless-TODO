dd0ea3f7f346c7c352d22db188bac3ab0aa1e501
seifElislam <seif.eleslam1990@gmail.com>
2020-01-19 01:21:32 +0200

    Refactor: Add separate todos data module that interact with dynamo

---
+++ backend/src/dataLayer/todosAccess.ts
@@ -0,0 +1,37 @@
+import * as AWS from 'aws-sdk';
+import * as AWSXRay from 'aws-xray-sdk';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { TodoItem } from "../models/TodoItem";
+
+const XAWS = AWSXRay.captureAWS(AWS);
+
+export class TodosAccess {
+  constructor(
+      private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+      private readonly todosTable = process.env.TODOS_TABLE,
+      private readonly indexName = process.env.INDEX_NAME
+  ) {}
+
+  async createTodo(todoItem: TodoItem): Promise<TodoItem> {
+      await this.docClient.put({
+          TableName: this.todosTable,
+          Item: todoItem
+      }).promise();
+      return todoItem;
+  }
+
+  async getAllTodos(userId: string): Promise<TodoItem[]> {
+      const result = await this.docClient.query({
+          TableName: this.todosTable,
+          IndexName: this.indexName,
+          KeyConditionExpression: 'userId = :userId',
+          ExpressionAttributeValues: {
+              ':userId': userId
+          }
+      }).promise();
+
+      return result.Items as TodoItem[];
+  }
+
+
+}
