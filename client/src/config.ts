851172b0092e7e998bc086cbe66a55803212d69f
seifElislam <seif.eleslam1990@gmail.com>
2020-01-19 02:17:46 +0200

    Update config with API endpoint

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'py82o5rq6e'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
