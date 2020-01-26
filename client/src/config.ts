113ee45954256141d963ff46715ac5cc825fc681
seifElislam <seif.eleslam1990@gmail.com>
2020-01-26 02:42:32 +0200

    Upate Auth0 domain and clientID

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '3nun1lbtza'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'jfqyzrayu1'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'dev-ca-rtp87.auth0.com',            // Auth0 domain
-  clientId: 'LGFV1CqveQTSIzmbxaIsSs7ca6ckIZ7T',          // Auth0 client id
+  domain: 'dev-cksu441t.auth0.com',            // Auth0 domain
+  clientId: 'tDnb471Er6lQj94ok2EYqO1I1PFh3pCt',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
